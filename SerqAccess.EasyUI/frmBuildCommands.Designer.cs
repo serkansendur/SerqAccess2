1ffbea711e614ea4550657611f6e38b8e5c613b0
Serkan Sendur <serkan_sendur@hotmail.com>
2015-11-17 15:29:32 +0200

    Generating SQL Commands

--- SerqAccess.EasyUI/frmBuildCommands.Designer.cs
+++ SerqAccess.EasyUI/frmBuildCommands.Designer.cs
@@ -28,11 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlConnectionString = new System.Windows.Forms.Panel();
+            this.lblSelectStatement = new System.Windows.Forms.Label();
+            this.txtSelectStatement = new System.Windows.Forms.TextBox();
+            this.btnBuildCommands = new System.Windows.Forms.Button();
+            this.txtCommandText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // pnlConnectionString
+            // 
+            this.pnlConnectionString.Location = new System.Drawing.Point(9, 12);
+            this.pnlConnectionString.Name = "pnlConnectionString";
+            this.pnlConnectionString.Size = new System.Drawing.Size(794, 31);
+            this.pnlConnectionString.TabIndex = 18;
+            this.pnlConnectionString.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlConnectionString_Paint);
+            // 
+            // lblSelectStatement
+            // 
+            this.lblSelectStatement.AutoSize = true;
+            this.lblSelectStatement.Location = new System.Drawing.Point(12, 56);
+            this.lblSelectStatement.Name = "lblSelectStatement";
+            this.lblSelectStatement.Size = new System.Drawing.Size(91, 13);
+            this.lblSelectStatement.TabIndex = 19;
+            this.lblSelectStatement.Text = "Select Statement:";
+            // 
+            // txtSelectStatement
+            // 
+            this.txtSelectStatement.Location = new System.Drawing.Point(15, 72);
+            this.txtSelectStatement.Multiline = true;
+            this.txtSelectStatement.Name = "txtSelectStatement";
+            this.txtSelectStatement.Size = new System.Drawing.Size(788, 110);
+            this.txtSelectStatement.TabIndex = 20;
+            // 
+            // btnBuildCommands
+            // 
+            this.btnBuildCommands.Location = new System.Drawing.Point(15, 188);
+            this.btnBuildCommands.Name = "btnBuildCommands";
+            this.btnBuildCommands.Size = new System.Drawing.Size(180, 23);
+            this.btnBuildCommands.TabIndex = 21;
+            this.btnBuildCommands.Text = "Build Commands";
+            this.btnBuildCommands.UseVisualStyleBackColor = true;
+            this.btnBuildCommands.Click += new System.EventHandler(this.btnBuildCommands_Click);
+            // 
+            // txtCommandText
+            // 
+            this.txtCommandText.Location = new System.Drawing.Point(15, 217);
+            this.txtCommandText.Multiline = true;
+            this.txtCommandText.Name = "txtCommandText";
+            this.txtCommandText.Size = new System.Drawing.Size(788, 110);
+            this.txtCommandText.TabIndex = 22;
+            // 
+            // frmBuildCommands
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 400);
+            this.Controls.Add(this.txtCommandText);
+            this.Controls.Add(this.btnBuildCommands);
+            this.Controls.Add(this.txtSelectStatement);
+            this.Controls.Add(this.lblSelectStatement);
+            this.Controls.Add(this.pnlConnectionString);
+            this.Name = "frmBuildCommands";
             this.Text = "frmBuildCommands";
+            this.Load += new System.EventHandler(this.frmBuildCommands_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlConnectionString;
+        private System.Windows.Forms.Label lblSelectStatement;
+        private System.Windows.Forms.TextBox txtSelectStatement;
+        private System.Windows.Forms.Button btnBuildCommands;
+        private System.Windows.Forms.TextBox txtCommandText;
     }
 }
--- SerqAccess.EasyUI/frmBuildCommands.cs
+++ SerqAccess.EasyUI/frmBuildCommands.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,8 @@ namespace SerqAccess.EasyUI
 {
     public partial class frmBuildCommands : Form
     {
+        private ctrlConnectionString _ctrlConnectionString = new ctrlConnectionString();
+        private bool _ctrlLoaded = false;
         private static frmBuildCommands _instance;
         public static frmBuildCommands GetInstance()
         {
@@ -21,5 +25,29 @@ namespace SerqAccess.EasyUI
         {
             InitializeComponent();
         }
+
+        private void frmBuildCommands_Load(object sender, EventArgs e)
+        {
+            if (!_ctrlLoaded)
+            {
+                pnlConnectionString.Controls.Add(_ctrlConnectionString);
+                _ctrlLoaded = true;
+            }
+        }
+
+        private void pnlConnectionString_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnBuildCommands_Click(object sender, EventArgs e)
+        {
+            string SQL = txtSelectStatement.Text;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings[_ctrlConnectionString.SelectedConnectionString].ConnectionString;
+           
+        }
+
+        
     }
 }
